d59d72a83975709b8512b3543ed07c5bad4e21c1
freshLi <13352019331@163.com>
2017-07-12 12:40:01 +0800

    flag

---
+++ flag.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+//	"fmt"
+	"basicSortAlgorithm/src/li/interfaces"
+)
+
+func main() {
+	//var numbers3 = [5]int{1, 2, 3, 4, 5}
+	//arrLen := len(numbers3)
+	//fmt.Println(arrLen)
+	//slice3 := numbers3[2 : arrLen]
+	//slice4 := numbers3[2:4]
+	//len1 := len(slice4)
+	//fmt.Println(len1)
+	//fmt.Println(cap(slice4))
+	//fmt.Println(slice4)
+	//length := 3
+	//capacity := 3
+	//fmt.Println(length)
+	//fmt.Println(capacity)
+	//fmt.Printf("%v, %v\n", (length == len(slice3)), (capacity == cap(slice3)))
+
+	interfaces.Test()
+}
