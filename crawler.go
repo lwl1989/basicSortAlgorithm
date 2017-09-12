4d4686f7a320da16609f3ec8d361d0735e563896
li <li@li>
2017-09-12 15:12:04 +0800

    crawler test

---
+++ crawler.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"container/list"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"strconv"
+	"github.com/fern4lvarez/go-metainspector/metainspector"
+	"gopkg.in/mgo.v2"
+	"time"
+)
+
+//url队列
+type urls struct {
+	got  list.List
+	want list.List
+}
+
+//爬虫任务
+type crawlerUrl struct {
+	Name     string   //任务名
+	MaxLen   int      //最大长度
+	MaxTime  int      //最大执行时间
+	Interval int      //自动重启时间
+	Inlet    *url.URL //入口
+	Urls     urls
+}
+
+//json
+type reqCrawler struct {
+	Name     string `json:"name"`
+	MaxLen   string `json:"maxLen"`
+	MaxTime  string `json:"maxTime"`
+	Interval string `json:"interval"`
+	Url      string `json:"url"`
+}
+
+type crawlerSave struct {
+	name string
+//	data struct{
+		url string
+		author string
+		keywords []string
+		describe string
+		images []string
+		time time.Time
+//	}
+}
+
+
+func Crawler(w http.ResponseWriter, req *http.Request) {
+	//var capacity int64
+	//buf := bytes.NewBuffer(make([]byte, 0, capacity))
+	//n , _ := buf.ReadFrom(req.Body)
+	//println(n)
+	//println(string(buf.Bytes()))
+
+	body, _ := ioutil.ReadAll(req.Body)
+	println("body")
+	body_str := string(body)
+	var reqCrawler reqCrawler
+	var crawler crawlerUrl
+	if "" != body_str {
+		if err := json.Unmarshal(body, &reqCrawler); err == nil {
+			println("crawler")
+
+			crawler.Inlet, err = url.Parse(reqCrawler.Url)
+			if nil != err {
+				io.WriteString(w, "over!\n")
+				panic("over")
+			}
+			crawler.MaxLen, _ = strconv.Atoi(reqCrawler.MaxLen)
+			crawler.MaxTime, _ = strconv.Atoi(reqCrawler.MaxTime)
+			crawler.Interval, _ = strconv.Atoi(reqCrawler.Interval)
+			crawler.Name = reqCrawler.Name
+			crawler.Urls.want.PushFront(crawler.Inlet)
+			mi := crawler.run()
+			crawler.save(mi)
+		} else {
+			println("err")
+			fmt.Println(err)
+		}
+	}
+
+	println("over")
+	io.WriteString(w, "hello, world!\n")
+}
+
+func (crawler *crawlerUrl) save(mi *metainspector.MetaInspector)  {
+	session, err := mgo.Dial("54.222.155.203:56790,54.222.182.136:56790,54.223.193.154:56790")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("crawler").C("test")
+
+	err = c.Insert(&crawlerSave{
+		crawler.Name,
+		crawler.Inlet.String(),
+		mi.Author(),
+		mi.Keywords(),
+		mi.Description(),
+		mi.Images(),
+		time.Now(),
+	})
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(err)
+	}
+}
+func (crawler *crawlerUrl) run() (*metainspector.MetaInspector) {
+
+	MI, err := metainspector.New(crawler.Inlet.String())
+	if err != nil {
+		//fmt.Println(MI)
+		fmt.Println("err:",err)
+		return nil
+	}
+	return MI
+}
+
+func main() {
+	http.HandleFunc("/", Crawler)
+	err := http.ListenAndServe(":9091", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
